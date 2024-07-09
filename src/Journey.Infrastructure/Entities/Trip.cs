219a2edff59d5fe745eef5a01e572bdcc7441dd4
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-07-09 11:17:07 -0300

    Aula 1 completa

--- src/Journey.Infrastructure/Entities/Trip.cs
+++ src/Journey.Infrastructure/Entities/Trip.cs
@@ -2,8 +2,8 @@
 public class Trip
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty; // não vai ser nulo e sim vazio
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public IList<Activity> Activities { get; set; } = [];
+    public IList<Activity> Activities { get; set; } = []; // não vai ser nulo e sim vazio
 }
